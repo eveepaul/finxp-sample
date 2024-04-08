d751b7791e6f84c661c2de9129fa756239e7c4a8
Yves Cabaneros <yves@MacBook-Pro.local>
2024-04-08 14:35:34 +0800

    feat: benefits sample component

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FINXP sample page
+
+
+### sample images links
+- https://unsplash.com/photos/macbook-pro-on-top-of-table-vSchPA-YA_A
+- https://unsplash.com/photos/person-using-laptop-computer-beside-aloe-vera-tMI2_-r5Nfo
+- https://unsplash.com/photos/person-using-laptop-computer-Hcfwew744z4
--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Button } from '@/components/shadcn/ui/button';
+import { GlobeAltIcon, Square3Stack3DIcon, CurrencyEuroIcon } from '@heroicons/vue/24/outline';
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import { Button } from '@/components/shadcn/ui/button';
             class="flex-grow flex items-center justify-center  iban4u h-[300px]"
         >
             <div
-                class="container flex flex-col items-center w-4/6 text-slate-50 ">
+                class="container flex flex-col items-center w-4/6 gap-4 text-slate-50">
                 <h1 class="font-bold text-8xl">IBAN4U</h1>
                 <h3 class="text-4xl">veritatis ducimus alias maxime dolore</h3>
                 <p class="text-xl text-center">Lorem ipsum, dolor sit amet
@@ -22,8 +23,33 @@ import { Button } from '@/components/shadcn/ui/button';
                 >Open an Account</Button>
             </div>
         </section>
-        <section class="h-[298px] p-8 bg-[#07076d]">
-            asdasd
+        <section
+            class="h-[298px] p-8 bg-[#07076d] flex justify-center gap-8 items-center"
+        >
+            <div
+                class="w-[400px] bg-slate-50 rounded-lg h-[180px] flex flex-col items-center justify-center">
+                <GlobeAltIcon class="w-16 h-16" />
+                <p class="px-10 pt-3 text-center">Lorem ipsum dolor sit amet
+                    consectetur
+                    adipisicing elit. Vel
+                    numquam</p>
+            </div>
+            <div
+                class="w-[400px] bg-slate-50 rounded-lg h-[180px] flex flex-col items-center justify-center">
+                <Square3Stack3DIcon class="w-16 h-16" />
+                <p class="px-10 pt-3 text-center">Lorem ipsum dolor sit amet
+                    consectetur
+                    adipisicing elit. Vel
+                    numquam</p>
+            </div>
+            <div
+                class="w-[400px] bg-slate-50 rounded-lg h-[180px] flex flex-col items-center justify-center">
+                <CurrencyEuroIcon class="w-16 h-16" />
+                <p class="px-10 pt-3 text-center">Lorem ipsum dolor sit amet
+                    consectetur
+                    adipisicing elit. Vel
+                    numquam</p>
+            </div>
         </section>
     </main>
 </template>
